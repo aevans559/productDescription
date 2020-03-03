2de28817c92420af135fbee2f7b21c062170b98a
Aaron Evans <aaronmevans14@gmail.com>
2020-03-03 08:51:12 -0600

    updated changes to my .gitignore file and switched App to ProductDescription

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import ProductDescription from './components/ProductDescription.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<ProductDescription/>, document.getElementById('root'));
